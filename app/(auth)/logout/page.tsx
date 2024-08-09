0c5e52f0dcdc6c4ce336403f89dc890a4cfcde13
Swaraj2004 <swarajchavan2004@gmail.com>
2024-08-09 23:25:01 +0530

    Fix logout

--- app/(auth)/logout/page.tsx
+++ app/(auth)/logout/page.tsx
@@ -1,15 +1,26 @@
-import { supabaseServer } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
+'use client';
 
-const LogoutPage = async () => {
-  const supabase = supabaseServer();
+import { supabaseClient } from '@/utils/supabase/client';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
-  const { error: signOutError } = await supabase.auth.signOut();
-  if (signOutError) {
-    return;
-  }
+const LogoutPage = () => {
+  const router = useRouter();
 
-  redirect('/');
+  useEffect(() => {
+    const logout = async () => {
+      const supabase = supabaseClient();
+      const { error: signOutError } = await supabase.auth.signOut();
+      if (!signOutError) {
+        router.push('/');
+        setTimeout(() => {
+          window.location.reload();
+        }, 100);
+      }
+    };
+
+    logout();
+  }, [router]);
 };
 
 export default LogoutPage;
