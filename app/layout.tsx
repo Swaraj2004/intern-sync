c5f6a012260500d24517d622a92b9236f3c9bd2b
Swaraj2004 <swarajchavan2004@gmail.com>
2024-08-24 12:59:12 +0530

    Update default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,7 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
           disableTransitionOnChange
         >
           <UserProvider>{children}</UserProvider>
