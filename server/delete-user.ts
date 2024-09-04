e900886ce7713380b7ce95e7c3a06361dde6159a
Swaraj2004 <swarajchavan2004@gmail.com>
2024-09-04 22:13:56 +0530

    Update delete-user

--- server/delete-user.ts
+++ server/delete-user.ts
@@ -1,11 +1,45 @@
 'use server';
 
+import { getRoles } from '@/app/helpers';
 import { supabaseAdmin } from '@/utils/supabase/admin';
+import { supabaseServer } from '@/utils/supabase/server';
+
+type Roles = { id: string; name: string }[];
 
 export default async function deleteUserById(authId: string) {
+  const supabase = supabaseServer();
   const supabaseAdminClient = supabaseAdmin();
 
   try {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+      throw new Error('User not authorized.');
+    }
+
+    const { data, error: rolesError } = await getRoles();
+    if (rolesError) {
+      throw new Error(rolesError.details);
+    }
+
+    const roles: Roles = data;
+    const roleMap = roles.reduce((acc, { name, id }) => {
+      acc[name] = id;
+      return acc;
+    }, {} as Record<string, string>);
+    const roleIds = user.user_metadata.role_ids as string[];
+
+    const hasRole =
+      roleIds.includes(roleMap['institute-coordinator']) ||
+      roleIds.includes(roleMap['department-coordinator']) ||
+      roleIds.includes(roleMap['college-mentor']);
+
+    if (!hasRole) {
+      throw new Error('User does not have the required role.');
+    }
+
     const { data: existingUser, error: getUserError } =
       await supabaseAdminClient.auth.admin.getUserById(authId);
 
