d3d41c3b224d19aea1e13e9896d5cd098640d2e8
Swaraj2004 <swarajchavan2004@gmail.com>
2024-09-26 11:34:50 +0530

    Update dashboard layout

--- app/dashboard/(roles)/layout.tsx
+++ app/dashboard/(roles)/layout.tsx
@@ -8,8 +8,10 @@ const DashboardNav = ({
 }>) => {
   return (
     <div className="lg:flex">
-      <div className="h-14 lg:h-screen bg-card lg:p-4 lg:w-2/5 xl:w-[30%] lg:max-w-72 lg:overflow-auto shadow-md">
-        <Navbar />
+      <div className="relative h-14 lg:h-screen lg:w-2/5 xl:w-[30%] lg:max-w-72 lg:overflow-auto">
+        <div className="fixed w-full lg:w-fit lg:p-4 lg:h-screen bg-card shadow-md">
+          <Navbar />
+        </div>
       </div>
       <RolesProvider>
         <div className="p-4 lg:px-8 lg:py-6 w-full max-w-[1250px] mx-auto overflow-auto">
